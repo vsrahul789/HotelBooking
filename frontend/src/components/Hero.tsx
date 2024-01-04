80d7c9ffbc0709bd20a3505d03ab768377e4512c
VSRahul789 <sairahul.k64@gmail.com>
2024-01-05 01:28:02 +0530

    some minor modification in file system

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="bg-sky-600 py-3 pb-20">
+      <div className="container mx-auto px-60">
+        <h1 className="text-5xl text-white font-bold -tracking-tight select-none">
+          Find your comfort...
+        </h1>
+        <p className="text-white font-semibold text-2xl mt-3 select-none">
+          In the most affordable and high quality hotels
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
